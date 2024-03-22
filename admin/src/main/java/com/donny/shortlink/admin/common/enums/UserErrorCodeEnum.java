bf1adf44c21ada7ed956ff59eb215c7595910c71
DonnyJin <1171314644@qq.com>
2024-03-22 17:55:47 +0800

    feature: 开发用户自定义异常码

---
+++ admin/src/main/java/com/donny/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.donny.shortlink.admin.common.enums;
+
+import com.donny.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+/**
+ * 用户错误码
+ * 
+ */
+public enum UserErrorCodeEnum implements IErrorCode {
+
+    USER_NULL("B000200", "用户记录不存在"),
+
+    USER_NAME_EXIST("B000201", "用户名已存在"),
+
+    USER_EXIST("B000202", "用户记录已存在"),
+
+    USER_SAVE_ERROR("B000203", "用户记录新增失败");
+
+    private final String code;
+
+    private final String message;
+
+    UserErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
