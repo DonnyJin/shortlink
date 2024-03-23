e39ce83691af142ea0d937b13702a9b93fb725c8
DonnyJin <1171314644@qq.com>
2024-03-23 19:51:46 +0800

    feature: 用户表通过ShardingSphere完成分表处理

---
+++ admin/src/test/java/com/donny/shortlink/admin/test/UserTableShardingTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.donny.shortlink.admin.test;
+
+public class UserTableShardingTest {
+
+    public static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
+            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
+            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
+            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
+            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
+            "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
+            "  `deletion_time` bigint(20) DEFAULT NULL COMMENT '注销时间戳',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1715030926162935810 DEFAULT CHARSET=utf8mb4;";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+            System.out.printf((SQL) + "%n", i);
+        }
+    }
+}
