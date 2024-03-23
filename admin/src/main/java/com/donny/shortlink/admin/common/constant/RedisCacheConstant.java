7c048de25c7a04888cc25a8f3333d16c85841f39
DonnyJin <1171314644@qq.com>
2024-03-23 19:50:49 +0800

    feature: 通过分布式锁完善用户注册功能，防止恶意请求段时间使用统一未注册用户名注册新用户

---
+++ admin/src/main/java/com/donny/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.donny.shortlink.admin.common.constant;
+
+/**
+ * 短链接后管 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    /**
+     * 用户注册分布式锁
+     */
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
+
+    /**
+     * 用户登录缓存标识
+     */
+    public static final String USER_LOGIN_KEY = "short-link:login:";
+}
