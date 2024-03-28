385204872dbadcb6585af535bc949c442823afb1
DonnyJin <1171314644@qq.com>
2024-03-28 19:37:28 +0800

    feature: 短链接后管系统调用中台请求相关接口

---
+++ admin/src/main/java/com/donny/shortlink/admin/controller/ShortLinkController.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.donny.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.donny.shortlink.admin.common.convention.result.Result;
+import com.donny.shortlink.admin.common.convention.result.Results;
+import com.donny.shortlink.admin.remote.ShortLinkActualRemoteService;
+import com.donny.shortlink.admin.remote.dto.req.ShortLinkBatchCreateReqDTO;
+import com.donny.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.donny.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.donny.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
+import com.donny.shortlink.admin.remote.dto.resp.ShortLinkBaseInfoRespDTO;
+import com.donny.shortlink.admin.remote.dto.resp.ShortLinkBatchCreateRespDTO;
+import com.donny.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.donny.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.donny.shortlink.admin.toolkit.EasyExcelWebUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 短链接后管控制层
+ */
+@RestController(value = "shortLinkControllerByAdmin")
+@RequiredArgsConstructor
+public class ShortLinkController {
+
+    private final ShortLinkActualRemoteService shortLinkActualRemoteService;
+
+    /**
+     * 创建短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+        return shortLinkActualRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 批量创建短链接
+     */
+    @SneakyThrows
+    @PostMapping("/api/short-link/admin/v1/create/batch")
+    public void batchCreateShortLink(@RequestBody ShortLinkBatchCreateReqDTO requestParam, HttpServletResponse response) {
+        Result<ShortLinkBatchCreateRespDTO> shortLinkBatchCreateRespDTOResult = shortLinkActualRemoteService.batchCreateShortLink(requestParam);
+        if (shortLinkBatchCreateRespDTOResult.isSuccess()) {
+            List<ShortLinkBaseInfoRespDTO> baseLinkInfos = shortLinkBatchCreateRespDTOResult.getData().getBaseLinkInfos();
+            EasyExcelWebUtil.write(response, "批量创建短链接-SaaS短链接系统", ShortLinkBaseInfoRespDTO.class, baseLinkInfos);
+        }
+    }
+
+    /**
+     * 修改短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkActualRemoteService.updateShortLink(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/page")
+    public Result<Page<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return shortLinkActualRemoteService.pageShortLink(requestParam.getGid(), requestParam.getOrderTag(), requestParam.getCurrent(), requestParam.getSize());
+    }
+}
