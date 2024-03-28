32ff98fa51e9762b796d05e8e9e13fd8c7d08d1d
DonnyJin <1171314644@qq.com>
2024-03-28 19:35:08 +0800

    feature: 开发短链接分页查询功能

---
+++ project/src/main/java/com/donny/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.donny.shortlink.project.dto.resp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 短链接分页返回参数
+ */
+@Data
+public class ShortLinkPageRespDTO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUri;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 有效期类型 0：永久有效 1：自定义
+     */
+    private Integer validDateType;
+
+    /**
+     * 有效期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 描述
+     */
+    private String describe;
+
+    /**
+     * 网站标识
+     */
+    private String favicon;
+
+    /**
+     * 历史PV
+     */
+    private Integer totalPv;
+
+    /**
+     * 今日PV
+     */
+    private Integer todayPv;
+
+    /**
+     * 历史UV
+     */
+    private Integer totalUv;
+
+    /**
+     * 今日UV
+     */
+    private Integer todayUv;
+
+    /**
+     * 历史UIP
+     */
+    private Integer totalUip;
+
+    /**
+     * 今日UIP
+     */
+    private Integer todayUip;
+}
