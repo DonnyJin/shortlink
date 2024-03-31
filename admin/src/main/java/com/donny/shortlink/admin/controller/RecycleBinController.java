3bc35b12563af81c669ed9f0daa2e1a83b3520a9
DonnyJin <1171314644@qq.com>
2024-03-31 19:49:19 +0800

    feature: 开发短链接新增回收站功能

---
+++ admin/src/main/java/com/donny/shortlink/admin/controller/RecycleBinController.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.donny.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.donny.shortlink.admin.common.convention.result.Result;
+import com.donny.shortlink.admin.common.convention.result.Results;
+import com.donny.shortlink.admin.dto.req.RecycleBinRecoverReqDTO;
+import com.donny.shortlink.admin.dto.req.RecycleBinRemoveReqDTO;
+import com.donny.shortlink.admin.dto.req.RecycleBinSaveReqDTO;
+import com.donny.shortlink.admin.remote.ShortLinkActualRemoteService;
+import com.donny.shortlink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
+import com.donny.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.donny.shortlink.admin.service.RecycleBinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 回收站管理控制层
+ */
+@RestController(value = "recycleBinControllerByAdmin")
+@RequiredArgsConstructor
+public class RecycleBinController {
+
+    private final RecycleBinService recycleBinService;
+    private final ShortLinkActualRemoteService shortLinkActualRemoteService;
+
+    /**
+     * 保存回收站
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/save")
+    public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
+        shortLinkActualRemoteService.saveRecycleBin(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 分页查询回收站短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/recycle-bin/page")
+    public Result<Page<ShortLinkPageRespDTO>> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam) {
+        return recycleBinService.pageRecycleBinShortLink(requestParam);
+    }
+
+    /**
+     * 恢复短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/recover")
+    public Result<Void> recoverRecycleBin(@RequestBody RecycleBinRecoverReqDTO requestParam) {
+        shortLinkActualRemoteService.recoverRecycleBin(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 移除短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/remove")
+    public Result<Void> removeRecycleBin(@RequestBody RecycleBinRemoveReqDTO requestParam) {
+        shortLinkActualRemoteService.removeRecycleBin(requestParam);
+        return Results.success();
+    }
+}
---
+++ admin/src/main/java/com/donny/shortlink/admin/service/impl/RecycleBinServiceImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.donny.shortlink.admin.service.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.donny.shortlink.admin.common.biz.user.UserContext;
+import com.donny.shortlink.admin.common.convention.exception.ServiceException;
+import com.donny.shortlink.admin.common.convention.result.Result;
+import com.donny.shortlink.admin.dao.entity.GroupDO;
+import com.donny.shortlink.admin.dao.mapper.GroupMapper;
+import com.donny.shortlink.admin.remote.ShortLinkActualRemoteService;
+import com.donny.shortlink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
+import com.donny.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.donny.shortlink.admin.service.RecycleBinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * URL 回收站接口实现层
+ */
+@Service(value = "recycleBinServiceImplByAdmin")
+@RequiredArgsConstructor
+public class RecycleBinServiceImpl implements RecycleBinService {
+
+    private final ShortLinkActualRemoteService shortLinkActualRemoteService;
+    private final GroupMapper groupMapper;
+
+    @Override
+    public Result<Page<ShortLinkPageRespDTO>> pageRecycleBinShortLink(ShortLinkRecycleBinPageReqDTO requestParam) {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .eq(GroupDO::getDelFlag, 0);
+        List<GroupDO> groupDOList = groupMapper.selectList(queryWrapper);
+        if (CollUtil.isEmpty(groupDOList)) {
+            throw new ServiceException("用户无分组信息");
+        }
+        requestParam.setGidList(groupDOList.stream().map(GroupDO::getGid).toList());
+        return shortLinkActualRemoteService.pageRecycleBinShortLink(requestParam.getGidList(), requestParam.getCurrent(), requestParam.getSize());
+    }
+}
