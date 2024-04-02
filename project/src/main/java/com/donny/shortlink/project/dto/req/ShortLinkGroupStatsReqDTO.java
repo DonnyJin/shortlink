49c0e7d30d381bf88edf0b41300e0d47d22cff66
DonnyJin <1171314644@qq.com>
2024-04-02 20:22:02 +0800

    feature: 开发单个短链接访问监控详情功能

---
+++ project/src/main/java/com/donny/shortlink/project/dto/req/ShortLinkGroupStatsReqDTO.java
@@ -0,0 +1,25 @@
+package com.donny.shortlink.project.dto.req;
+
+import lombok.Data;
+
+/**
+ * 分组短链接监控请求参数
+ */
+@Data
+public class ShortLinkGroupStatsReqDTO {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 开始日期
+     */
+    private String startDate;
+
+    /**
+     * 结束日期
+     */
+    private String endDate;
+}
---
+++ project/src/main/java/com/donny/shortlink/project/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
@@ -0,0 +1,36 @@
+package com.donny.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 短链接基础访问监控响应参数
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsAccessDailyRespDTO {
+
+    /**
+     * 日期
+     */
+    private String date;
+
+    /**
+     * 访问量
+     */
+    private Integer pv;
+
+    /**
+     * 独立访客数
+     */
+    private Integer uv;
+
+    /**
+     * 独立IP数
+     */
+    private Integer uip;
+}
