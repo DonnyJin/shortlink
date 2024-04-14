8f24c24ef7d0e1baf26916457e7040ca66008a94
DonnyJin <1171314644@qq.com>
2024-04-14 22:16:02 +0800

    optimize: 优化用户不存在异常

--- admin/src/main/java/com/donny/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/donny/shortlink/admin/service/UserService.java
@@ -1,18 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.donny.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
@@ -30,6 +15,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 根据用户名查询用户信息
+     *
      * @param username 用户名
      * @return 用户返回实体
      */
@@ -37,6 +23,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 查询用户名是否存在
+     *
      * @param username 用户名
      * @return 用户名存在返回 True，不存在返回 False
      */
@@ -44,18 +31,21 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 注册用户
+     *
      * @param requestParam 注册用户请求参数
      */
     void register(UserRegisterReqDTO requestParam);
 
     /**
      * 根据用户名修改用户
+     *
      * @param requestParam 修改用户请求参数
      */
     void update(UserUpdateReqDTO requestParam);
 
     /**
      * 用户登录
+     *
      * @param requestParam 用户登录请求参数
      * @return 用户登录返回参数 Token
      */
@@ -63,6 +53,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 检查用户是否登录
+     *
      * @param username 用户名
      * @param token    用户登录 Token
      * @return 用户是否登录标识
@@ -71,6 +62,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 退出登录
+     *
      * @param username 用户名
      * @param token    用户登录 Token
      */
