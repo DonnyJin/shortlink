efd093ea20cff27bb1927b47d2543974969b1c19
DonnyJin <1171314644@qq.com>
2024-04-15 21:37:40 +0800

    feature: 短链接中台创建短链接接口请求限制

---
+++ project/src/main/java/com/donny/shortlink/project/config/SentinelRuleConfig.java
@@ -0,0 +1,28 @@
+package com.donny.shortlink.project.config;
+
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 初始化限流配置
+ */
+@Component
+public class SentinelRuleConfig implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule createOrderRule = new FlowRule();
+        createOrderRule.setResource("create_short-link");
+        createOrderRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        createOrderRule.setCount(3500);
+        rules.add(createOrderRule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
---
+++ project/src/main/java/com/donny/shortlink/project/handler/CustomBlockHandler.java
@@ -0,0 +1,16 @@
+package com.donny.shortlink.project.handler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.donny.shortlink.project.common.convention.result.Result;
+import com.donny.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.donny.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+
+/**
+ * 自定义流控策略
+ */
+public class CustomBlockHandler {
+
+    public static Result<ShortLinkCreateRespDTO> createShortLinkBlockHandlerMethod(ShortLinkCreateReqDTO requestParam, BlockException exception) {
+        return new Result<ShortLinkCreateRespDTO>().setCode("B100000").setMessage("当前访问网站人数过多，请稍后再试...");
+    }
+}
