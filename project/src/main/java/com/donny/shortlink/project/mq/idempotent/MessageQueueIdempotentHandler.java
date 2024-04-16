1cfb8ee1a75506a8b0f7dbfac228c34e5ae12833
DonnyJin <1171314644@qq.com>
2024-04-16 21:02:57 +0800

    feature: 短链接监控消费者幂等逻辑处理

---
+++ project/src/main/java/com/donny/shortlink/project/mq/idempotent/MessageQueueIdempotentHandler.java
@@ -0,0 +1,67 @@
+package com.donny.shortlink.project.mq.idempotent;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 消息队列幂等处理器
+ */
+@Component
+@RequiredArgsConstructor
+public class MessageQueueIdempotentHandler {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private static final String IDEMPOTENT_KEY_PREFIX = "short-link:idempotent:";
+
+    private static final String IDEMPOTENT_CONSUMED = "1";
+
+    private static final String IDEMPOTENT_PROCESSED = "0";
+
+
+    /**
+     * 判断当前消息是否消费过
+     *
+     * @param messageId 消息唯一标识
+     * @return 消息是否消费过
+     */
+    public boolean isMessageProcessed(String messageId) {
+        String key = IDEMPOTENT_KEY_PREFIX + messageId;
+        return Boolean.TRUE.equals(stringRedisTemplate.opsForValue().setIfAbsent(key, IDEMPOTENT_PROCESSED, 2, TimeUnit.MINUTES));
+    }
+
+    /**
+     * 判断消息消费流程是否执行完成
+     *
+     * @param messageId 消息唯一标识
+     * @return 消息是否执行完成
+     */
+    public boolean isAccomplish(String messageId) {
+        String key = IDEMPOTENT_KEY_PREFIX + messageId;
+        return Objects.equals(stringRedisTemplate.opsForValue().get(key), IDEMPOTENT_CONSUMED);
+    }
+
+    /**
+     * 设置消息流程执行完成
+     *
+     * @param messageId 消息唯一标识
+     */
+    public void setAccomplish(String messageId) {
+        String key = IDEMPOTENT_KEY_PREFIX + messageId;
+        stringRedisTemplate.opsForValue().set(key, IDEMPOTENT_CONSUMED, 2, TimeUnit.MINUTES);
+    }
+
+    /**
+     * 如果消息处理遇到异常情况，删除幂等标识
+     *
+     * @param messageId 消息唯一标识
+     */
+    public void delMessageProcessed(String messageId) {
+        String key = IDEMPOTENT_KEY_PREFIX + messageId;
+        stringRedisTemplate.delete(key);
+    }
+}
