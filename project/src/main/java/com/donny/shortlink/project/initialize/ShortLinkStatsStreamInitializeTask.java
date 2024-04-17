4dcd2fb071e088c78465f7992a1a22846be4d829
DonnyJin <1171314644@qq.com>
2024-04-17 23:20:29 +0800

    optimize: 初始化 Redis Stream Topic 和消费者组

---
+++ project/src/main/java/com/donny/shortlink/project/initialize/ShortLinkStatsStreamInitializeTask.java
@@ -0,0 +1,26 @@
+package com.donny.shortlink.project.initialize;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import static com.donny.shortlink.project.common.constant.RedisKeyConstant.*;
+
+/**
+ * 初始化短链接监控消息队列消费者组
+ */
+@Component
+@RequiredArgsConstructor
+public class ShortLinkStatsStreamInitializeTask implements InitializingBean {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Boolean hasKey = stringRedisTemplate.hasKey(SHORT_LINK_STATS_STREAM_TOPIC_KEY);
+        if (hasKey == null || !hasKey) {
+            stringRedisTemplate.opsForStream().createGroup(SHORT_LINK_STATS_STREAM_TOPIC_KEY, SHORT_LINK_STATS_STREAM_GROUP_KEY);
+        }
+    }
+}
