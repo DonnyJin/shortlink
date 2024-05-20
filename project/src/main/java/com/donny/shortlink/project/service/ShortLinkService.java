62035b94c6f0c6095b81f8d8ecb9eed841bfb772
DonnyJin <1171314644@qq.com>
2024-05-20 14:17:57 +0800

    feature: 添加短链接从创建相关与个别DTO

--- project/src/main/java/com/donny/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/donny/shortlink/project/service/ShortLinkService.java
@@ -1,18 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.donny.shortlink.project.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -39,6 +24,7 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
 
     /**
      * 创建短链接
+     *
      * @param requestParam 创建短链接请求参数
      * @return 短链接创建信息
      */
@@ -46,6 +32,7 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
 
     /**
      * 根据分布式锁创建短链接
+     *
      * @param requestParam 创建短链接请求参数
      * @return 短链接创建信息
      */
@@ -53,6 +40,7 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
 
     /**
      * 批量创建短链接
+     *
      * @param requestParam 批量创建短链接请求参数
      * @return 批量创建短链接返回参数
      */
@@ -60,12 +48,14 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
 
     /**
      * 修改短链接
+     *
      * @param requestParam 修改短链接请求参数
      */
     void updateShortLink(ShortLinkUpdateReqDTO requestParam);
 
     /**
      * 分页查询短链接
+     *
      * @param requestParam 分页查询短链接请求参数
      * @return 短链接分页返回结果
      */
@@ -73,6 +63,7 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
 
     /**
      * 查询短链接分组内数量
+     *
      * @param requestParam 查询短链接分组内数量请求参数
      * @return 查询短链接分组内数量响应
      */
@@ -80,6 +71,7 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
 
     /**
      * 短链接跳转
+     *
      * @param shortUri 短链接后缀
      * @param request  HTTP 请求
      * @param response HTTP 响应
@@ -88,6 +80,7 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
 
     /**
      * 短链接统计
+     *
      * @param fullShortUrl         完整短链接
      * @param gid                  分组标识
      * @param shortLinkStatsRecord 短链接统计实体参数
