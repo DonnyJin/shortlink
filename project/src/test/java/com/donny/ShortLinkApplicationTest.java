6fa0f1c22247f83a25dc589e67c5945553f15a21
DonnyJin <1171314644@qq.com>
2024-10-08 16:36:43 +0800

    test

---
+++ project/src/test/java/com/donny/ShortLinkApplicationTest.java
@@ -0,0 +1,12 @@
+package com.donny;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShortLinkApplicationTest {
+
+    public static void main(String[] args) {
+//        FileInputStream fis = new FileInputStream();
+//        fis.transferTo()
+    }
+}
